722236dd17dada90b2b3e069a7a9e4154d473628
hananeltassa <hananeltassa15@gmail.com>
2025-01-08 21:21:10 +0200

    Updated Users Page

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
+import UsersTable from "../../components/UsersTable";
 import { Box, Typography } from "@mui/material";
 
-const Dashboard = () => {
+const Users = () => {
   return (
     <DashboardLayout>
-      <Box>
-        <Typography variant="h4">Users</Typography>
-        <Typography variant="body1">This is the Users page.</Typography>
+      <Box sx={{ p: 3 }}>
+        {/* Page Title */}
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          Users
+        </Typography>
+
+        {/* Description */}
+        <Typography variant="body1" sx={{ mb: 3 }}>
+          Manage all registered users below.
+        </Typography>
+
+        {/* Users Table */}
+        <UsersTable />
       </Box>
     </DashboardLayout>
   );
 };
 
-export default Dashboard;
+export default Users;
