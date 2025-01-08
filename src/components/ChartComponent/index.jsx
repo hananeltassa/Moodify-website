a82d0f75b1b98e84f275a29c9d7f64ec2dbf495f
hananeltassa <hananeltassa15@gmail.com>
2025-01-08 21:20:15 +0200

    Updated Chart component

--- src/components/ChartComponent/index.jsx
+++ src/components/ChartComponent/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { PieChart } from "@mui/x-charts";
 import { Card, CardContent, Typography, Box, Stack } from "@mui/material";
 
-const ChartComponent = ({ data, title }) => {
+const ChartComponent = ({ data = [], title }) => {
+  if (!Array.isArray(data)) {
+    console.error("Invalid data provided to ChartComponent. Expected an array.");
+    data = [];
+  }
+
   return (
     <Card
       sx={{
@@ -14,7 +19,10 @@ const ChartComponent = ({ data, title }) => {
     >
       <CardContent>
         {/* Title */}
-        <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2,}}>
+        <Typography
+          variant="h6"
+          sx={{ fontWeight: "bold", mb: 2, textAlign: "center" }}
+        >
           {title}
         </Typography>
 
@@ -24,35 +32,43 @@ const ChartComponent = ({ data, title }) => {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: 250,
+            height: 300, // Ensure enough height to center
           }}
         >
-          <PieChart series={[{ data }]} width={300} height={300} />
+          {data.length > 0 ? (
+            <PieChart series={[{ data }]} width={300} height={300} />
+          ) : (
+            <Typography variant="body2" color="text.secondary">
+              No data available
+            </Typography>
+          )}
         </Box>
 
         {/* Custom Legend */}
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-          mt={3}
-        >
-          {data.map((item) => (
-            <Box key={item.id} sx={{ display: "flex", alignItems: "center" }}>
-              <Box
-                sx={{
-                  width: 12,
-                  height: 12,
-                  borderRadius: "50%",
-                  backgroundColor: item.color,
-                  mr: 1,
-                }}
-              ></Box>
-              <Typography variant="body2">{item.id}</Typography>
-            </Box>
-          ))}
-        </Stack>
+        {data.length > 0 && (
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            mt={3}
+          >
+            {data.map((item) => (
+              <Box key={item.id} sx={{ display: "flex", alignItems: "center" }}>
+                <Box
+                  sx={{
+                    width: 12,
+                    height: 12,
+                    borderRadius: "50%",
+                    backgroundColor: item.color,
+                    mr: 1,
+                  }}
+                ></Box>
+                <Typography variant="body2">{item.id}</Typography>
+              </Box>
+            ))}
+          </Stack>
+        )}
       </CardContent>
     </Card>
   );
