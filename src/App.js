f4f24b8557cf25442e8c852187fb4687f1be289b
hananeltassa <hananeltassa15@gmail.com>
2025-01-08 16:51:15 +0200

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SplashScreen from "./components/SplashScreen";
 import Login from "./pages/Login";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
 import useSplashScreen from "./hooks/useSplashScreen"; 
 import "./styles/styles.css";
 
@@ -16,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </Router>
   );
