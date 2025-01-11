fcf4b4274db5d30740299972dd25759d4f2effe1
hananeltassa <hananeltassa15@gmail.com>
2025-01-11 21:13:49 +0200

    Updated redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './slices/usersSlice';
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    auth: authReducer,
   },
 });
 
